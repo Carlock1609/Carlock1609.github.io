366c3616087dd82f0f8b7836286af7255d4f93e8
Jon <53983746+Carlock1609@users.noreply.github.com>
2020-02-22 23:41:52 -0800

    updates

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,22 @@
 console.log('connected')
+
+// Get the modal
+let modal = document.querySelector("#myModal");
+// Get the image
+let images = document.querySelectorAll("#myImg");
+for(let i=0; i<images.length; i++) {
+  images[i].addEventListener("click", function() {
+    images.id = "myImg"
+    modal.style.display = "block";
+    modalImg.src = this.src;
+  })
+}
+
+// BUG THIS GOES WITH THE MODAL
+let modalImg = document.querySelector("#img01");
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+span.addEventListener('click', function() {
+  modal.style.display = "none";
+}) 
+

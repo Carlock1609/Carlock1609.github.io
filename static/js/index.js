408b4e5a02496573dc7596c5f70457eac7a65df3
Jonathan Yates <carlock1609@gmail.com>
2020-08-26 10:36:10 -0700

    updates

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,13 @@
 // Scrolling affects
 ScrollReveal().reveal('#profile-img', { delay: 100 });
 ScrollReveal().reveal('#caption', { interval: 200 });
-ScrollReveal().reveal('#summary', { delay: 300 });
-ScrollReveal().reveal('#credentials', { delay: 300 });
+ScrollReveal().reveal('.summary-text', { delay: 300 });
+ScrollReveal().reveal('.credentials-text', { delay: 300 });
+ScrollReveal().reveal('#separator-projects', { delay: 300 });
+ScrollReveal().reveal('#projects *', { delay: 300 });
+ScrollReveal().reveal('#separator-education', { delay: 300 });
+ScrollReveal().reveal('#link-div *', { delay: 300 });
+ScrollReveal().reveal('#certificates *', { delay: 300 });
 
 
 // MODAL

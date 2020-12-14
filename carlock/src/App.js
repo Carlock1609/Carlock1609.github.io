ef9c10fee290b82b0cab067bf91c0cab827bfb6b
Carlock1609 <carlock1609@gmail.com>
2020-12-14 09:21:44 -0800

    base new updates

---
+++ carlock/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Switch, Router as BrowserRouter, Route } from 'react-router'
+
+import './App.css';
+
+const App = (props) => {
+  return (
+    <Switch>
+
+    </Switch>
+  );
+}
+
+export default App;

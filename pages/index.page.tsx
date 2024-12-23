f298945b9783ad08156ce48beaecdde05ee2998d
jyates-cpts <jyates@crosslinktax.com>
2024-12-22 16:42:37 -0800

    base script build

---
+++ pages/index.page.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from "next";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
+
+const Home: NextPage = () => {
+  // const router = useRouter();
+  // useEffect(() => {
+  //   router.push("/users");
+  // }, []);
+
+  return <>Hello World</>;
+};
+
+export default Home;
